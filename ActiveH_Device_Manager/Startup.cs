3ec8300fcee338bae719cac3c2eda2cea6e6693d
Jonathan <jonathan@alpine-software.co.uk>
2017-03-31 08:02:29 +0100

    Add project files.

---
+++ ActiveH_Device_Manager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ActiveH_Device_Manager.Startup))]
+namespace ActiveH_Device_Manager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ ActiveH_Device_Manager/WSHttpHandler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Web;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.WebSockets;
+using System.Web.WebSockets;
+
+namespace ActiveH_Device_Manager
+{
+    public class WSHttpHandler : IHttpHandler
+    {
+        WebSocketConnectionManager wscm = new WebSocketConnectionManager();
+
+        public bool IsReusable
+        {
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.IsWebSocketRequest)
+            {
+                context.AcceptWebSocketRequest(WebSocketRequestHandler);
+            }
+        }
+
+        public async Task WebSocketRequestHandler(AspNetWebSocketContext webSocketContext)
+        {
+            WebSocket webSocket = webSocketContext.WebSocket;
+
+            wscm.AddSocket(webSocket);
+
+            const int maxMsgSize = 1024;
+
+            ArraySegment<Byte> recdDataBuffer = new ArraySegment<Byte>(new Byte[maxMsgSize]);
+            CancellationToken cancellationToken = new CancellationToken();
+
+            while (webSocket.State == WebSocketState.Open)
+            {
+                WebSocketReceiveResult wsRecResult = await webSocket.ReceiveAsync(recdDataBuffer, cancellationToken);
+
+                if (wsRecResult.MessageType == WebSocketMessageType.Close)
+                {
+                    string socketId = wscm.GetId(webSocket);
+                    await wscm.RemoveSocket(socketId);
+                    //await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, cancellationToken);
+                }
+                else
+                {
+                    byte[] payload = recdDataBuffer.Array.Where(b => b != 0).ToArray();
+                    string recString = Encoding.UTF8.GetString(payload, 0, payload.Length);
+                }
+            }
+        }
+    }
+}
